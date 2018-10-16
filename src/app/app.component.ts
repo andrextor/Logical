5e8c07733493aa9234b2ddd3604182d1679b04f5
Ivan Andres <andresgom11012@gmail.com>
2018-10-16 09:18:24 -0500

    pagina termina con responsive y navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageScrollConfig, PageScrollService, PageScrollInstance } from 'ng2-page-scroll';
+import * as $ from 'jquery';
+
+// import { element } from 'protractor';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Logical';
+export class AppComponent implements OnInit {
+  title = 'Logical Sistemas S.A.S';
+  // hash: HTMLImageElement; /* Defining element */
+
+// ...
+
+// this.element = document.createElement('img'); /*creating a img*/
+
+  public tiltes = [
+    {
+      titulo: 'Software',
+      imagen: 'service_1-min.jpg',
+    },
+    {
+      titulo: 'Aplicaciones Web',
+      imagen: 'service_2-min.jpg',
+    },
+    {
+      titulo: 'Diseño UX - UI',
+      imagen: 'service_3-min.jpg',
+    },
+    {
+      titulo: 'Aplicaciones Moviles',
+      imagen: 'service_4-min.jpg',
+    },
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    $(document).ready(function() {
+
+      $(window).scroll(function() {
+         // tslint:disable-next-line:no-var-keyword
+       // tslint:disable-next-line:prefer-const
+     let barra = $(window).scrollTop();
+       if (barra > 700) {
+         // tslint:disable-next-line:prefer-const
+         let posicion = barra * 0.10;
+        $('.main-soluciones').css({
+           'background-position': '0 -' + posicion + 'px'
+         } );
+       }
+     });
+
+    });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {Ng2PageScrollModule} from 'ng2-page-scroll';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    Ng2PageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
